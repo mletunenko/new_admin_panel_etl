866208e218287d73142de23963b2bf6371e50c59
mletunenko <mletunenko@gmail.com>
2025-02-01 09:14:24 +0300

    обработка исключения про обращении к сервису аутентификации

--- etl/app/movies/backends.py
+++ etl/app/movies/backends.py
@@ -1,11 +1,16 @@
 import http
 import json
+from datetime import datetime
 from enum import StrEnum, auto
+from logging import getLogger
 
 import requests
+from requests import ConnectionError
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
+
+logger = getLogger(__name__)
 
 User = get_user_model()
 
@@ -20,23 +25,27 @@ class CustomBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         url = settings.AUTH_LOGIN_URL
         payload = {'email': username, 'password': password}
-        response = requests.post(url, data=json.dumps(payload))
-        if response.status_code != http.HTTPStatus.OK:
-            return None
-        data = response.json()
+        try:
+            response = requests.post(url, data=json.dumps(payload))
+            if response.status_code != http.HTTPStatus.OK:
+                return None
+            data = response.json()
 
-        username = data.get('email')
-        is_staff = is_superuser = data['role'] in ['admin', 'superuser']
-        user, created = User.objects.get_or_create({"username": username,
-                                                    "is_staff": is_staff,
-                                                    "is_superuser": is_superuser},
-                                                   id=data['id'], )
-        if not created:
-            user.username = username
-            user.is_staff = is_staff
-            user.is_superuser = is_superuser
-            user.save()
-        return user
+            username = data.get('email')
+            is_staff = is_superuser = data['role'] in ['admin', 'superuser']
+            user, created = User.objects.get_or_create({"username": username,
+                                                        "is_staff": is_staff,
+                                                        "is_superuser": is_superuser},
+                                                       id=data['id'], )
+            if not created:
+                user.username = username
+                user.is_staff = is_staff
+                user.is_superuser = is_superuser
+                user.save()
+            return user
+        except ConnectionError as e:
+            logger.error(f'{datetime.now()} Не удалось установить соединение с Elasticsearch: {e}')
+            raise e
 
     def get_user(self, user_id):
         try:
